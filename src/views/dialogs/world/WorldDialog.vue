c1c5376383cedcd37e53fe55aa9c32c7e032be63
pa <maplenagisa@gmail.com>
2025-03-26 02:56:19 +0900

    refactor: WorldAllowedDomainsDialog.vue

--- src/views/dialogs/world/WorldDialog.vue
+++ src/views/dialogs/world/WorldDialog.vue
@@ -741,51 +741,18 @@
 
         <!--  Nested Hmm-->
         <!--  dialog: change Allowed Video Player Domains  -->
-        <el-dialog
-            :before-close="beforeDialogClose"
-            :visible.sync="worldAllowedDomainsDialog.visible"
-            :title="$t('dialog.allowed_video_player_domains.header')"
-            width="600px"
-            destroy-on-close
-            append-to-body
-            @mousedown.native="dialogMouseDown"
-            @mouseup.native="dialogMouseUp">
-            <div>
-                <el-input
-                    v-for="(domain, index) in worldAllowedDomainsDialog.urlList"
-                    :key="index"
-                    v-model="worldAllowedDomainsDialog.urlList[index]"
-                    :value="domain"
-                    size="small"
-                    style="margin-top: 5px">
-                    <el-button
-                        slot="append"
-                        icon="el-icon-delete"
-                        @click="worldAllowedDomainsDialog.urlList.splice(index, 1)"></el-button>
-                </el-input>
-                <el-button size="mini" style="margin-top: 5px" @click="worldAllowedDomainsDialog.urlList.push('')">
-                    {{ $t('dialog.allowed_video_player_domains.add_domain') }}
-                </el-button>
-            </div>
-            <template #footer>
-                <el-button
-                    type="primary"
-                    size="small"
-                    :disabled="!worldAllowedDomainsDialog.worldId"
-                    @click="saveWorldAllowedDomains">
-                    {{ $t('dialog.allowed_video_player_domains.save') }}
-                </el-button>
-            </template>
-        </el-dialog>
+        <world-allowed-domains-dialog :world-allowed-domains-dialog.sync="worldAllowedDomainsDialog" />
     </el-dialog>
 </template>
 
 <script>
     import utils from '../../../classes/utils';
     import database from '../../../repository/database.js';
-    import { worldRequest } from '../../../classes/request';
+    import WorldAllowedDomainsDialog from './WorldAllowedDomainsDialog.vue';
+
     export default {
         name: 'WorldDialog',
+        components: { WorldAllowedDomainsDialog },
         inject: [
             'API',
             'showUserDialog',
@@ -979,22 +946,6 @@
                 D.worldId = this.worldDialog.id;
                 D.urlList = this.worldDialog.ref?.urlList ?? [];
                 D.visible = true;
-            },
-            saveWorldAllowedDomains() {
-                const D = this.worldAllowedDomainsDialog;
-                worldRequest
-                    .saveWorld({
-                        id: D.worldId,
-                        urlList: D.urlList
-                    })
-                    .then((args) => {
-                        this.$message({
-                            message: 'Allowed Video Player Domains updated',
-                            type: 'success'
-                        });
-                        return args;
-                    });
-                D.visible = false;
             }
         }
     };
