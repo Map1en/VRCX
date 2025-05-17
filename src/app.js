7fd3b0c361a0581a706a43874e8a1f1d3ea42ac0
pa <maplenagisa@gmail.com>
2025-05-17 21:15:33 +0900

    fix: Error When Creating Group Members/Plus Instance

--- src/app.js
+++ src/app.js
@@ -9179,9 +9179,6 @@ $app.methods.createNewInstance = async function (worldId = '', options) {
         params.queueEnabled = D.queueEnabled;
         if (D.groupAccessType === 'members') {
             params.roleIds = D.roleIds;
-            params.canRequestInvite = true;
-        } else if (D.groupAccessType === 'plus') {
-            params.canRequestInvite = true;
         }
     }
     if (
